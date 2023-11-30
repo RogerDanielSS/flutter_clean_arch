f60c4c265459fb93cad924ee49cc833236555192
RogerDanielSS <danielsimoesp@outlook.com>
2023-11-30 15:28:43 -0300

    change auth use case to receive a model

--- lib/domain/use_cases/authentication.dart
+++ lib/domain/use_cases/authentication.dart
@@ -1,7 +1,12 @@
-import 'package:flutter/material.dart';
-
 import '../entities/account_entity.dart';
 
 abstract class Authentication{
-  Future<AccountEntity> auth({required String email, required String password}); // Retorn a future, cuz it's async
+  Future<AccountEntity> auth(AuthenticationParams params); // Retorn a future, cuz it's async
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({required this.email, required this.password});
 }
