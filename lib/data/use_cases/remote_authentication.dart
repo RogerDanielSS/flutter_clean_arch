530be41b4c57085e36da7dc9478cc48fb892d61a
RogerDanielSS <danielsimoesp@outlook.com>
2023-12-08 20:56:30 -0300

    move domain details to data model

--- lib/data/use_cases/remote_authentication.dart
+++ lib/data/use_cases/remote_authentication.dart
@@ -1,36 +1,41 @@
-
-import 'package:flutter_clean_arch/domain/entities/account_entity.dart';
-
+import '../../domain/entities/entities.dart';
 import '../../domain/helpers/helpers.dart';
 import '../../domain/use_cases/authentication.dart';
 
 import '../http/http.dart';
+import '../models/models.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
 
-  RemoteAuthentication({required this.httpClient,  required this.url});
+  RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<AccountEntity> auth(AuthenticationParams params ) async {
+  Future<AccountEntity> auth(AuthenticationParams params) async {
     try {
-      final json = await httpClient.request(url: url, method: 'post', body:  
-      RemoteAuthenticationParams.fromDomain(params).toJson());
+      final json = await httpClient.request(
+          url: url,
+          method: 'post',
+          body: RemoteAuthenticationParams.fromDomain(params).toJson());
 
-      return AccountEntity.fromJson(json);
+      return RemoteAccountModel.fromJson(json).toEntity();
     } on HttpError catch (error) {
-      throw error == HttpError.unauthorized ? DomainError.invalidCredentials : DomainError.unexpected;
+      throw error == HttpError.unauthorized
+          ? DomainError.invalidCredentials
+          : DomainError.unexpected;
     }
   }
 }
+
 class RemoteAuthenticationParams {
   final String email;
   final String password;
 
   RemoteAuthenticationParams({required this.email, required this.password});
 
-  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) => 
-    RemoteAuthenticationParams(email: params.email, password: params.password);
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(
+          email: params.email, password: params.password);
 
-  Map toJson () => {'email': email, 'password': password};
+  Map toJson() => {'email': email, 'password': password};
 }
