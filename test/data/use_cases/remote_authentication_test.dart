cb57d33852846d2715e67b2d79eca8b480e2e44a
RogerDanielSS <danielsimoesp@outlook.com>
2023-12-20 10:20:18 -0300

    update remote auth test to mocktail

--- test/data/use_cases/remote_authentication_test.dart
+++ test/data/use_cases/remote_authentication_test.dart
@@ -1,6 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
 import 'package:flutter_clean_arch/domain/helpers/helpers.dart';
 import 'package:flutter_clean_arch/domain/use_cases/use_cases.dart';
@@ -9,14 +9,14 @@ import 'package:flutter_clean_arch/data/use_cases/use_cases.dart';
 import 'package:flutter_clean_arch/data/http/http.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {
-  @override
-  Future<Map> request(
-      {required String? url, required String? method, Map? body}) async {
-    return super.noSuchMethod(
-      Invocation.method(#request, [url, method, body]),
-      returnValue: Future<Map>.value({}),
-    );
-  }
+  // @override
+  // Future<Map> request(
+  //     {required String? url, required String? method, Map? body}) async {
+  //   return super.noSuchMethod(
+  //     Invocation.method(#request, [url, method, body]),
+  //     returnValue: Future<Map>.value({}),
+  //   );
+  // }
 }
 
 void main() {
@@ -30,7 +30,7 @@ void main() {
 
   // helpers
 
-  PostExpectation mockRequest() => when(httpClient.request(
+  When mockRequest() => when(() => httpClient.request(
       url: url,
       method: 'post',
       body: RemoteAuthenticationParams.fromDomain(params).toJson()));
@@ -52,10 +52,10 @@ void main() {
     sut.auth(params);
 
     // assert
-    verify(httpClient.request(
+    verify(() => httpClient.request(
         url: url,
         method: 'post',
-        body: RemoteAuthenticationParams.fromDomain(params).toJson()));
+        body: RemoteAuthenticationParams.fromDomain(params).toJson())).called(1);
   });
 
   test('Should return UnexpectedError if HttpClient returns error 400',
