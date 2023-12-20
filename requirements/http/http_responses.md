b2ef7309d8d06b2cbe69d93b400afb6687e41a39
RogerDanielSS <danielsimoesp@outlook.com>
2023-12-20 10:24:18 -0300

    ensure http_adapter calls post with correct url, headers and body

--- requirements/http/http_responses.md
+++ requirements/http/http_responses.md
@@ -1,9 +1,9 @@
  # HTTP 
 
 > ## Sucesso
-1. Requesto com verbo http válido (post)
-2. Passar nos headers o content type JSON
-3. OK - 200 e resposta com dados
+1. ✅ Request com verbo http válido (post)
+2. ✅ Passar nos headers o content type JSON
+3. ✅ OK - 200 e resposta com dados
 4. No content - 204 e resposta sem dados
 
 > ## Erros
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -2,52 +2,67 @@ import 'dart:convert';
 
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 class ClientSpy extends Mock implements Client {
-  // @override
-  // Future<Response> post(Uri url, {Object? body, Encoding? encoding, Map<String, String>? headers}) async {
-  //   return super.noSuchMethod(
-  //     Invocation.method(
-  //       #post,
-  //       [url],
-  //       {
-  //         #body: body,
-  //         #encoding: encoding,
-  //         #headers: headers,
-  //       },
-  //     ),
-  //     returnValue: Future.value(Response('', 200)), // Mock a successful response
-  //   );
-  // }
-}
+  ClientSpy() {
+    mockPost(200);
+  }
 
+  When mockPostCall() => when(() => this
+      .post(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+
+  void mockPost(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockPostCall().thenAnswer((_) async => Response(body, statusCode));
+
+  void mockPostError() =>
+      when(() => mockPostCall().thenThrow(Exception()));
+}
 
 class HttpAdapter {
   Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request({
-    required String url, 
-    required String method
-  }) async {
-    await client.post(Uri.parse(url));
+  Future<void> request(
+      {required String url, required String method, Map? body}) async {
+    await client.post(Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        },
+        body: body);
   }
 }
 
-
 void main() {
+  late HttpAdapter sut;
+  late ClientSpy client;
+  late String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+  });
+
+  setUpAll(() {
+    url = faker.internet.httpUrl();
+    registerFallbackValue(Uri.parse(url));
+  });
+
   group('post', () {
+    const body = {'any_key': 'any_value'};
+
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpsUrl();
+      await sut.request(url: url, method: 'post', body: body);
 
-      await sut.request(url: url, method: 'post');  
-
-      verify(client.post(Uri.parse(url)));
+      verify(() => client.post(Uri.parse(url),
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: body)).called(1);
     });
   });
 }
