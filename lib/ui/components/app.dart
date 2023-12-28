55d440e14ff579a10fae9fa1535f1c7069840470
RogerDanielSS <danielsimoesp@outlook.com>
2023-12-28 17:14:53 -0300

    wip: styled components

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -7,10 +7,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    const primaryColorDark = Color.fromRGBO(136, 0, 39, 1);
+    const primaryColorLight = Color.fromRGBO(188, 71, 213, 1);
+
+    return MaterialApp(
       title: 'MrRoganTano',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      theme: ThemeData(
+          primaryColor: primaryColor,
+          primaryColorDark: primaryColorDark,
+          primaryColorLight: primaryColorLight,
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(secondary: primaryColor, background: Colors.white)),
+      home: const LoginPage(),
     );
   }
 }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -8,28 +8,49 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            const Image(image: AssetImage('lib/ui/assets/logo.png')),
+            Container(
+                height: 240,
+                margin: const EdgeInsets.only(bottom: 32),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [
+                          Theme.of(context).primaryColorLight,
+                          Theme.of(context).primaryColorDark
+                        ]),
+                    boxShadow: const [
+                      BoxShadow(
+                          offset: Offset(0, 0),
+                          spreadRadius: 0,
+                          blurRadius: 4,
+                          color: Colors.black)
+                    ],
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(80))),
+                child:
+                    const Image(image: AssetImage('lib/ui/assets/logo.png'))),
             const Text('LOGIN'),
             Form(
               child: Column(
                 children: <Widget>[
                   TextFormField(
                     decoration: const InputDecoration(
-                        labelText: 'Email', 
-                        icon: Icon(Icons.email)
-                      ),
+                        labelText: 'Email', icon: Icon(Icons.email)),
                     keyboardType: TextInputType.emailAddress,
                   ),
                   TextFormField(
                     decoration: const InputDecoration(
-                        labelText: 'Senha', 
-                        icon: Icon(Icons.lock)
-                      ),
+                        labelText: 'Senha', icon: Icon(Icons.lock)),
                     obscureText: true,
                   ),
-                  ElevatedButton(onPressed: (){}, child: const Text('ENTRAR')),
-                  TextButton.icon(onPressed: (){}, icon: const Icon(Icons.person), label: const Text('CRIAR CONTA'))
+                  ElevatedButton(onPressed: () {}, child: const Text('ENTRAR')),
+                  TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('CRIAR CONTA'))
                 ],
               ),
             )
